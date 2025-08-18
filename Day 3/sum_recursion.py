d66485199c3047c01d240305f9c781c789476dd1
Achutha Venkata Satyanarayana <achuthavenkatasatyanarayana@gmail.com>
2025-08-18 09:17:18 +0530

    sum_recursion.py

--- Day 3/sum_recursion.py
+++ Day 3/sum_recursion.py
@@ -1,7 +1,6 @@
-#sum of the factorial
-def sum_fact(n):
-  if n != 0:
-    return n + sum_fact(n-1)
-  return 0
-
-sum_fact(5)
+def sum_list(i,num,n):
+  if i == n:
+    return 0
+  return num[i] + sum_list(i+1,num,n)
+n = [1,2,3,4,5]
+sum_list(0,n,len(n))
